70553b8568ddbfdf280f2703722cf1759138a474
Miro Miettinen <miro.miettinen@student.lab.fi>
2024-10-14 12:38:53 +0300

    -

--- src/app/feedback/feedback.component.spec.ts
+++ src/app/feedback/feedback.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FeedbackComponent } from './feedback.component';
+import { Router } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ FeedbackComponent ],
+      imports: [ RouterTestingModule.withRoutes([]) ],
       schemas: [
         CUSTOM_ELEMENTS_SCHEMA,
         NO_ERRORS_SCHEMA
@@ -18,10 +22,29 @@ describe('FeedbackComponent', () => {
 
     fixture = TestBed.createComponent(FeedbackComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate back to home component when cancel button is clicked', () => {
+    const routerSpy = spyOn(router, 'navigate');
+
+    const button = fixture.nativeElement.querySelector('button[type="button"]');
+    button.click();
+
+    expect(routerSpy).toHaveBeenCalledOnceWith(['home']);
+  });
+
+  it('should call cancel method when cancel button is clicked', () => {
+    const cancelSpy = spyOn(component, 'cancel');
+
+    const button = fixture.nativeElement.querySelector('button[type="button"]');
+    button.click();
+
+    expect(cancelSpy).toHaveBeenCalled();
   });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -9,9 +9,7 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ HomeComponent ],
-      schemas: [
-        CUSTOM_ELEMENTS_SCHEMA
-      ]
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
 
@@ -22,5 +20,26 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have correct h3 text', () => {
+    const h3Text = fixture.nativeElement.querySelector('h3')?.textContent;
+    expect(h3Text).toEqual('Angular unit testing tutorials');
+  });
+
+  it('should contain links to external websites', () => {
+    const links = fixture.nativeElement.querySelectorAll('mat-nav-list a'); //Testataan, että HTML sisältää linkkejä muille verkkosivuille
+
+    expect(links.length).toBe(3); // Varmistetaan, että linkkejä on kolme
+
+    const expectedLinks = [
+        'https://angular.io/guide/testing-components-scenarios',
+        'https://testing-angular.com/introduction/#introduction',
+        'https://simpleweblearning.com/form-testing-in-angular/'
+    ];
+
+    links.forEach((link: HTMLAnchorElement, index: number) => {
+        expect(link.href).toBe(expectedLinks[index]); 
+    });
   });
 });
